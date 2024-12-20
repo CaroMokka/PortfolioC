1efca85f65564305c63bd85d72fb81c5260263bf
carolina <ci.arayagonzalez@gmail.com>
2024-12-20 17:39:25 -0300

    Cambio de extensión en archivo video

--- src/components/Cover/Cover.jsx
+++ src/components/Cover/Cover.jsx
@@ -1,4 +1,4 @@
-import coverVideo from"../../assets/cover-02.mp4"
+import coverVideo from"../../assets/cover-02.mov"
 import nameTitle from "../../assets/name.png"
 import arterisco from"../../assets/arterisco.webp"
 import "./Cover.css"
